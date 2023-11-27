162b0c2a5c2720198753eb681b7cad2a08793a18
EdsonJSF <EdsonJSF1990@gmail.com>
2023-11-27 11:13:20 -0400

    Feat: Routes

--- src/HeoresApp.jsx
+++ src/HeoresApp.jsx
@@ -1,8 +1,9 @@
+import { MainRouter } from "./main-router";
+
 export const HeoresApp = () => {
   return (
     <>
-      <h1>HeoresApp</h1>
-      <hr />
+      <MainRouter />
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import './main.css'
-import { HeoresApp } from './HeoresApp'
+import "./main.css";
+import { HeoresApp } from "./HeoresApp";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HeoresApp></HeoresApp>
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <HeoresApp></HeoresApp>
+    </BrowserRouter>
+  </React.StrictMode>
+);
