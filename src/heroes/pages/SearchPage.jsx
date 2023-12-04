5434dff1535cae309b6598ecaf4c54d6075580b8
EdsonJSF <EdsonJSF1990@gmail.com>
2023-12-04 12:01:33 -0400

    Feat: Seach Create a function that handle submit form

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,27 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+
 import { HeroCard } from "../components";
+import { useForm } from "../../shared";
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = "" } = queryString.parse(location.search);
+
+  const { search, onInputChange, onResetForm } = useForm({ search: "" });
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+    const findSearch = search.trim().toLowerCase();
+
+    navigate(`?q=${findSearch}`);
+
+    onResetForm();
+  };
+
   return (
     <>
       <h1>Search</h1>
@@ -10,13 +31,15 @@ export const SearchPage = () => {
         <div className="col-5">
           <h4>Searchin</h4>
           <hr />
-          <form>
+          <form onSubmit={handleSearch}>
             <input
               type="text"
               placeholder="Search"
               className="form-control"
               name="search"
               autoComplete="off"
+              value={search}
+              onChange={onInputChange}
             />
             <button type="submit" className="btn btn-outline-primary mt-1">
               Search
@@ -30,7 +53,7 @@ export const SearchPage = () => {
 
           <div className="alert alert-primary">Search a Hero</div>
           <div className="alert alert-danger">
-            No Results with <b>search</b>
+            No Results with <b>{q}</b>
           </div>
 
           {/* <HeroCard /> */}
