185e1a6e28f75807866cd89cfbe84f601ebfd900
Edson Sánchez <edsonjsf1990@gmail.com>
2024-05-06 09:45:05 -0400

    feat: test on PublicRoute, authenticated page

--- tests/src/PublicRoute.test.jsx
+++ tests/src/PublicRoute.test.jsx
@@ -1,13 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import { PublicRoute } from "../../src/public-router";
 import { AuthContext } from "../../src/auth";
 
 describe("Test on <PublicRoute />", () => {
-  const contextValue = {
-    logged: false,
-  };
-
   test("should show the children if not authenticated", () => {
+    const contextValue = {
+      logged: false,
+    };
+
     render(
       <AuthContext.Provider value={contextValue}>
         <PublicRoute>
@@ -17,5 +19,37 @@ describe("Test on <PublicRoute />", () => {
     );
 
     expect(screen.getByText("Public Route")).toBeTruthy();
+  });
+
+  test("should show the private page if authenticated", () => {
+    const contextValue = {
+      logged: true,
+      user: {
+        id: 1,
+        name: "test name",
+      },
+    };
+
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter initialEntries={["/login"]}>
+          <Routes>
+            <Route
+              path="login"
+              element={
+                <PublicRoute>
+                  <h1>Public Route</h1>
+                </PublicRoute>
+              }
+            />
+            <Route path="marvel" element={<h1>Marvel Page</h1>} />
+
+            <Route path="/" element={<Navigate to="/marvel" />} />
+          </Routes>
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+
+    expect(screen.getByText("Marvel Page")).toBeTruthy();
   });
 });
